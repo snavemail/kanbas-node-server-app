5c05595d11374e242d0c8f399ade85c11b3861dd
snavemail <liam.jk.evans@gmail.com>
2024-04-18 02:42:07 -0400

    Add User model, schema, DAO, and routes

---
+++ Users/routes.js
@@ -0,0 +1,76 @@
+import * as userDao from './dao.js';
+
+export default function UserRoutes(app) {
+  const createUser = async (req, res) => {
+    const user = await userDao.createUser(req.body);
+    res.json(user);
+  };
+
+  const deleteUser = async (req, res) => {
+    const status = await userDao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+
+  const findAllUsers = async (req, res) => {
+    const users = await userDao.findAllUsers();
+    res.json(users);
+  };
+
+  const findUserById = async (req, res) => {};
+
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await userDao.updateUser(userId, req.body);
+    const currentUser = await userDao.findUserById(userId);
+    req.session['currentUser'] = currentUser;
+    res.json(status);
+  };
+
+  const signup = async (req, res) => {
+    const user = await userDao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json({ message: 'Username already taken' });
+      return;
+    }
+    const currentUser = await userDao.createUser(req.body);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser);
+  };
+
+  const signin = async (req, res) => {
+    const { username, password } = req.body;
+    const currentUser = await userDao.findUserByCredentials(username, password);
+
+    if (currentUser) {
+      req.session['currentUser'] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.json(200);
+  };
+
+  const profile = async (req, res) => {
+    const currentUser = req.session['currentUser'];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
+
+    res.json(currentUser);
+  };
+
+  app.post('/api/users', createUser);
+  app.get('/api/users', findAllUsers);
+  app.get('/api/users/:userId', findUserById);
+  app.put('/api/users/:userId', updateUser);
+  app.delete('/api/users/:userId', deleteUser);
+  app.post('/api/users/signup', signup);
+  app.post('/api/users/signin', signin);
+  app.post('/api/users/signout', signout);
+  app.post('/api/users/profile', profile);
+}
--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 import express from 'express';
-import CourseRoutes from './Kanbas/courses/routes.js';
-import ModuleRoutes from './Kanbas/modules/routes.js';
+import 'dotenv/config';
+import session from 'express-session';
 import Lab5 from './Lab5.js';
 import cors from 'cors';
+import CourseRoutes from './Kanbas/courses/routes.js';
+import ModuleRoutes from './Kanbas/modules/routes.js';
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+
+try {
+  mongoose.connect(CONNECTION_STRING);
+} catch (error) {
+  console.log(error);
+}
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true
+  };
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
+
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-app.listen(process.env.port || 4000);
+UserRoutes(app);
+
+app.listen(process.env.PORT || 4000);
